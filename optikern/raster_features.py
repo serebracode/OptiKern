7a74f306b1450c59e9ce683e4c2962db5bf7ff9f
Denis Serebryakov <159767889+serebracode@users.noreply.github.com>
2025-12-06 09:14:20 +0300

    Create raster_features.py

---
+++ optikern/raster_features.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class RasterEdgeProfile:
+    top: list
+    middle: list
+    bottom: list
+
+def build_raster_profiles(font, glyph_list, master):
+    return {}
